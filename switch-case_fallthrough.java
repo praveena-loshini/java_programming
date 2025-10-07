eba45ac550a97ff256e778df7ca94868db7447ad
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-07 22:30:32 +0530

    Create switch-case_fallthrough.java

---
+++ switch-case_fallthrough.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     switch(n){
+         case 1:
+             System.out.println("january");
+         case 2:
+             System.out.println("feb");
+         case 3:
+             System.out.println("mar");
+         case 4:
+             System.out.println("apr");
+         case 5:
+             System.out.println("may");
+         case 6:
+             System.out.println("jun");
+         case 7:
+             System.out.println("jul");
+         case 8:
+             System.out.println("aug");
+         case 9:
+             System.out.println("sep");
+         case 10:
+             System.out.println("oct");
+         case 11:
+             System.out.println("nov");
+         case 12:
+             System.out.println("dec");
+             
+     }
+
+}}
