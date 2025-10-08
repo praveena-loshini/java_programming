7fc18f0cc79ec39ef99964998d237d87ad546d43
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-08 18:54:01 +0530

    Create breakindowhileloop.java

---
+++ breakindowhileloop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        do{
+            String s1=s.nextLine();
+            s1=s1.toLowerCase();
+            if(s1.equals("exit")){
+                System.out.println("exiting");
+                break;
+            }
+        }while(true);
+        
+        
+        
+        
+    }
+}
