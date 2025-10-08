42f77add51717d6c31f5be92832dee141f5641d3
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-08 19:32:28 +0530

    Create system.exitvsreturn.java

---
+++ system.exitvsreturn.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        for(int i=0;i<2;i++){
+            if(i<2){
+                break;
+            }
+        }System.out.println("after loop");
+          for(int i=0;i<2;i++){
+            if(i<2){
+                System.exit(0);
+            }
+        }
+        System.out.println("after loop");
+        
+        }}
