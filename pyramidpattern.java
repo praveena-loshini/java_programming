d4ab858d301f1eb0b85b8912569af0d09e6d81af
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-08 21:01:37 +0530

    Create pyramidpattern.java

---
+++ pyramidpattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        }}
