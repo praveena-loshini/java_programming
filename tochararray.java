2c789cc709f2af586e541a739f86a9ef0bdcbe74
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-10 21:41:49 +0530

    Create tochararray.java

---
+++ tochararray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        char[] a=s1.toCharArray();
+        for(int i=0;i<a.length;i++){
+        System.out.println(a[i]);
+        }
+    }
+}
