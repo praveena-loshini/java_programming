6408c9286b6393f4d8df65d08f53d9cca52301e1
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-10 21:26:36 +0530

    Create autoboxingunboxing.java

---
+++ autoboxingunboxing.java
@@ -0,0 +1,13 @@
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        Integer a=100;
+        Integer b=100;
+        Integer c=1000;
+        Integer d=1000;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
