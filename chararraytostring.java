3bf249b13ed0944258232d3a329a71c1c4fa0c1b
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-10 21:51:16 +0530

    Create chararraytostring.java

---
+++ chararraytostring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        char[] arr={'j','a','v','a'};
+        String a=new String(arr);
+        System.out.println(a);
+    }
+}
