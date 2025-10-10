4685d060df842165b307dea2cdc909ba601a606e
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-10 20:28:56 +0530

    Create lossyconversions.java

---
+++ lossyconversions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        double a=s.nextDouble();
+        long b=(long)a;
+        double c=b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    
+    }
+}
