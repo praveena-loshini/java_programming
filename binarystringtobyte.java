4fac5f5a0cfa5b9fa922be8b9c5ede25e673868c
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-10 21:47:41 +0530

    Create binarystringtobyte.java

---
+++ binarystringtobyte.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        System.out.println(Byte.parseByte(s1,2));
+    }
+}
