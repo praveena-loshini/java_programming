10885ed87f6b2f2fd5b77a30bef2d116aef4f660
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-10 21:38:28 +0530

    Create valueofnewinteger.java

---
+++ valueofnewinteger.java
@@ -0,0 +1,14 @@
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=Integer.valueOf(100);
+        int b=Integer.valueOf(100);
+        System.out.println(a==b);
+        Integer a1=new Integer(100);
+        Integer b1=new Integer(100);
+        System.out.println(a1==b1);
+        
+    }
+}
