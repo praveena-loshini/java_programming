42141afd07b1e63fc1d645785ed35d5caa59bca7
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-12 18:54:02 +0530

    Create efficientstringconcatenation.java

---
+++ efficientstringconcatenation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+StringBuilder sb=new StringBuilder();
+for(int i=1;i<=n;i++){
+sb.append(Integer.toString(i));
+
+    }
+System.out.println(sb);
+}}
