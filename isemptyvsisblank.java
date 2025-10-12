982fc7fda250a59c1ac79d91fbc677767388835a
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-12 22:40:29 +0530

    Create isemptyvsisblank.java

---
+++ isemptyvsisblank.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String s1=" ";
+String s2="";
+System.out.println(s1.isEmpty());
+System.out.println(s1.isBlank());
+System.out.println(s2.isEmpty());
+System.out.println(s2.isBlank());
+
+
+    }
+}
