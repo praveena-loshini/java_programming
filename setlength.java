ff4358f12c7c1b79fb5fced943ebcc8b751fd24d
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-12 22:04:45 +0530

    Create setlength.java

---
+++ setlength.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main { public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String n=s.nextLine();
+StringBuilder sb=new StringBuilder(n);
+sb.setLength(4);
+System.out.println(sb);
+sb.setLength(10);
+System.out.println(sb);
+}}
