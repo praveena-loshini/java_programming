f15f722ee47adce9c67dbf14969329b5b133fcc5
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-12 22:10:12 +0530

    Create chainingappendcalls.java

---
+++ chainingappendcalls.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main { public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String name=s.nextLine();
+int age=s.nextInt();
+StringBuilder sb=new StringBuilder().append("Name:").append(name).append(",Age:").append(age);
+
+System.out.println(sb);
+}}
