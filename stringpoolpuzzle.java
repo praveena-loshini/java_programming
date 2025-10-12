e1e6fa57a8493a746f9f60e107b57f57d90e3c07
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-12 22:25:36 +0530

    Create stringpoolpuzzle.java

---
+++ stringpoolpuzzle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String s1="java";
+String s2="java";
+String s3=new String();
+s3="string";
+if(s1==s2){
+    System.out.println("true");
+}else{
+    System.out.println("false");
+}
+if(s1==s3){
+    System.out.println("true");
+}else{
+    System.out.println("false");
+}
+
+    }
+}
