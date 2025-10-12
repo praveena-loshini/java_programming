76a80d692c96fc601d4cb2b51378994a66bbb5ef
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-12 18:56:25 +0530

    Create Stringreversal.java

---
+++ Stringreversal.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String n=s.nextLine();
+StringBuilder sb=new StringBuilder(n);
+
+System.out.println(sb.reverse());
+}}
