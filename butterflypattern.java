43f8bd9b3fcf95d4d9df8c0a50c7d431ce5aa50e
praveena-loshini <praveenaloshini001@gmail.com>
2025-10-15 21:17:46 +0530

    Create butterflypattern.java

---
+++ butterflypattern.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
+    }
+    for(int j=1;j<=2*(n-i);j++){
+        System.out.print(" ");
+    }
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+for(int i=n-1;i>=1;i--){
+    for(int j=i;j>=1;j--){
+        System.out.print("*");
+    }
+    for(int j=1;j<=2*(n-i);j++){
+        System.out.print(" ");
+    }
+    for(int j=i;j>=1;j--){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+    }
+}
